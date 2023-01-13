ea18a19e15d9f1c62cd5162bc67cfb9e77295272
Abhijeet Mamidwar <abhijeet.mamidwar@sarvaha.com>
2023-01-13 17:46:32 +0530

    Sorting and splitting files done

---
+++ app.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const readStream = fs.createReadStream(__dirname + "/cards.txt", {
+  highWaterMark: 1024,
+});
+
+var fileNumber = 1,
+  remainFromPreviousFile = "",
+  lastCommaLocation = -1;
+
+var result = {};
+
+readStream.on("data", function (chunk) {
+  let writeStream = fs.createWriteStream(__dirname + `/f${fileNumber}.txt`);
+
+  let writeToFile = "";
+  let textToProcess = remainFromPreviousFile + chunk.toString();
+  let allWords = [];
+
+  lastCommaLocation = -1;
+
+  let finalVersion = "";
+  for (let i = 0; i < textToProcess.length; i++) {
+    if (
+      textToProcess[i].toLowerCase() != textToProcess[i].toUpperCase() ||
+      textToProcess[i] == ","
+    ) {
+      finalVersion += textToProcess[i].toLowerCase();
+    }
+  }
+
+  textToProcess = finalVersion;
+  for (let i = 0; i < textToProcess.length; i++) {
+    if (textToProcess[i] == ",") {
+      let word = textToProcess.slice(lastCommaLocation + 1, i);
+      if (word.length > 0) {
+        allWords.push(word);
+      }
+      lastCommaLocation = i;
+    }
+  }
+
+  allWords.sort();
+  for (let i = 0; i < allWords.length; i++) {
+    writeToFile += allWords[i] + ",";
+  }
+
+  if (lastCommaLocation != textToProcess.length - 1) {
+    remainFromPreviousFile = "";
+    remainFromPreviousFile += textToProcess.slice(lastCommaLocation + 1);
+  }
+
+  result[`F${fileNumber}.txt`] = {
+    minimum: allWords[0],
+    maximum: allWords[allWords.length - 1],
+  };
+
+  // console.log(allWords);
+  writeStream.write(writeToFile);
+  fileNumber += 1;
+});
+
+console.log(result);
