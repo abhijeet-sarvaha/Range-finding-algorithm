986a784242be59e3289ef753506e889e1a0c2ebd
Abhijeet Mamidwar <abhijeet.mamidwar@sarvaha.com>
2023-01-16 10:50:06 +0530

    Found min/max from file chunk

--- app.js
+++ app.js
@@ -7,53 +7,57 @@ var fileNumber = 1,
   remainFromPreviousFile = "",
   result = {};
 
-readStream.on("data", function (chunk) {
-  let writeStream = fs.createWriteStream(__dirname + `/f${fileNumber}.txt`);
+readStream
+  .on("data", function (chunk) {
+    let writeStream = fs.createWriteStream(__dirname + `/f${fileNumber}.txt`);
 
-  let writeToFile = "",
-    finalVersion = "",
-    allWords = [],
-    lastCommaLocation = -1,
-    textToProcess = remainFromPreviousFile + chunk.toString();
+    let writeToFile = "",
+      finalVersion = "",
+      allWords = [],
+      lastCommaLocation = -1,
+      textToProcess = remainFromPreviousFile + chunk.toString();
 
-  for (let i = 0; i < textToProcess.length; i++) {
-    if (
-      textToProcess[i].toLowerCase() != textToProcess[i].toUpperCase() ||
-      textToProcess[i] == ","
-    ) {
-      finalVersion += textToProcess[i].toLowerCase();
-    }
-  }
-
-  textToProcess = finalVersion;
-  for (let i = 0; i < textToProcess.length; i++) {
-    if (textToProcess[i] == ",") {
-      let word = textToProcess.slice(lastCommaLocation + 1, i);
-      if (word.length > 0) {
-        allWords.push(word);
+    for (let i = 0; i < textToProcess.length; i++) {
+      if (
+        textToProcess[i].toLowerCase() != textToProcess[i].toUpperCase() ||
+        textToProcess[i] == ","
+      ) {
+        finalVersion += textToProcess[i].toLowerCase();
       }
-      lastCommaLocation = i;
     }
-  }
 
-  allWords.sort();
-  for (let i = 0; i < allWords.length; i++) {
-    writeToFile += allWords[i] + ",";
-  }
+    textToProcess = finalVersion;
+    for (let i = 0; i < textToProcess.length; i++) {
+      if (textToProcess[i] == ",") {
+        let word = textToProcess.slice(lastCommaLocation + 1, i);
+        if (word.length > 0) {
+          allWords.push(word);
+        }
+        lastCommaLocation = i;
+      }
+    }
 
-  if (lastCommaLocation != textToProcess.length - 1) {
-    remainFromPreviousFile = "";
-    remainFromPreviousFile += textToProcess.slice(lastCommaLocation + 1);
-  }
+    allWords.sort();
+    for (let i = 0; i < allWords.length; i++) {
+      writeToFile += allWords[i] + ",";
+    }
 
-  result[`F${fileNumber}.txt`] = {
-    minimum: allWords[0],
-    maximum: allWords[allWords.length - 1],
-  };
+    if (lastCommaLocation != textToProcess.length - 1) {
+      remainFromPreviousFile = "";
+      remainFromPreviousFile += textToProcess.slice(lastCommaLocation + 1);
+    }
 
-  // console.log(allWords);
-  writeStream.write(writeToFile);
-  fileNumber += 1;
-});
+    result[`F${fileNumber}`] = {
+      minimum: allWords[0],
+      maximum: allWords[allWords.length - 1],
+    };
 
-console.log(result);
+    // console.log(allWords[0]);
+    writeStream.write(writeToFile);
+    fileNumber += 1;
+
+    // console.log(result);
+  })
+  .on("end", function (req, res) {
+    console.log(result);
+  });
