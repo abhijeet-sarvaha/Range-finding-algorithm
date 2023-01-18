399b10d6d25de224c99c27bb22d2514a8c3daa98
Abhijeet Mamidwar <abhijeet.mamidwar@sarvaha.com>
2023-01-18 14:21:24 +0530

    Working algo but gives incorrect for few conditions

--- app.js
+++ app.js
@@ -5,73 +5,53 @@ const readStream = fs.createReadStream(__dirname + "/cards.txt", {
 
 function splitDataIntoChunks() {
   let fileNumber = 1,
-    remainFromPreviousFile = "",
-    result1 = [];
+    remainFromPreviousFile = "";
 
-  readStream
-    .on("data", function (chunk) {
-      let writeToFile = "",
-        finalVersion = "",
-        allWords = [],
-        lastCommaLocation = -1,
-        textToProcess = remainFromPreviousFile + chunk.toString();
+  readStream.on("data", function (chunk) {
+    let finalVersion = "",
+      allWords = [],
+      lastCommaLocation = -1,
+      textToProcess = remainFromPreviousFile + chunk.toString();
 
-      // Get all words in lowercase
-      for (let i = 0; i < textToProcess.length; i++) {
-        if (
-          textToProcess[i].toLowerCase() != textToProcess[i].toUpperCase() ||
-          textToProcess[i] == ","
-        ) {
-          finalVersion += textToProcess[i].toLowerCase();
-        }
+    // Get all words in lowercase
+    for (let i = 0; i < textToProcess.length; i++) {
+      if (
+        textToProcess[i].toLowerCase() != textToProcess[i].toUpperCase() ||
+        textToProcess[i] == ","
+      ) {
+        finalVersion += textToProcess[i].toLowerCase();
       }
+    }
 
-      // Get each word and add to array
-      textToProcess = finalVersion;
-      for (let i = 0; i < textToProcess.length; i++) {
-        if (textToProcess[i] == ",") {
-          let word = textToProcess.slice(lastCommaLocation + 1, i);
-          if (word.length > 0) {
-            allWords.push(word);
-          }
-          lastCommaLocation = i;
+    // Get each word and add to array
+    textToProcess = finalVersion;
+    for (let i = 0; i < textToProcess.length; i++) {
+      if (textToProcess[i] == ",") {
+        let word = textToProcess.slice(lastCommaLocation + 1, i);
+        if (word.length > 0) {
+          allWords.push(word);
         }
+        lastCommaLocation = i;
       }
+    }
 
-      // Sort allWords in an array
-      allWords.sort();
-      const set = new Set(allWords);
-      allWords = [...set];
+    // Sort allWords in an array
+    allWords.sort();
+    const set = new Set(allWords);
+    allWords = [...set];
 
-      // Get slice from data
-      if (lastCommaLocation != textToProcess.length - 1) {
-        remainFromPreviousFile = "";
-        remainFromPreviousFile += textToProcess.slice(lastCommaLocation + 1);
-      }
+    // Get slice from data
+    if (lastCommaLocation != textToProcess.length - 1) {
+      remainFromPreviousFile = "";
+      remainFromPreviousFile += textToProcess.slice(lastCommaLocation + 1);
+    }
 
-      // Update result1 data
-      // add min/max from chunk to result1
-      result1.push([
-        allWords[0],
-        allWords[allWords.length - 1],
-        allWords.length,
-      ]);
-
-      let writeStream = fs.createWriteStream(
-        __dirname + `/Phase1/f${fileNumber}_${allWords.length}.json`
-      );
-      writeStream.write(JSON.stringify(allWords));
-      fileNumber += 1;
-    })
-    .on("end", function () {
-      fs.appendFileSync(
-        __dirname + `/result1.json`,
-        JSON.stringify(result1),
-        (err) => {
-          console.log(err);
-        }
-      );
-    });
+    let writeStream = fs.createWriteStream(
+      __dirname + `/Phase1/f${fileNumber}_${allWords.length}.json`
+    );
+    writeStream.write(JSON.stringify(allWords));
+    fileNumber += 1;
+  });
 }
 
 ///////// Split chunks of data into group of 30 words each  /////////
@@ -83,7 +63,7 @@ function multiSplit() {
   var currentIndex = [0, 0, 0, 0];
 
   const block = 30,
-    endIndex = [72, 77, 77, 4],
+    endIndex = [59, 63, 57, 4],
     result2 = [[], [], [], []];
 
   while (conditionChecker(currentIndex, endIndex)) {
@@ -95,19 +75,7 @@ function multiSplit() {
       );
 
       if (data[0] !== undefined && data.length > 0) {
-        result2[i].push([
-          data[0],
-          data[data.length - 1],
-          `f${i + 1}_${data.length}`,
-        ]);
-
-        // let writeStream = fs.createWriteStream(
-        //   __dirname +
-        //     `/Phase1/f${i + 1}_${data.length}_${currentIndex[i]}-${
-        //       currentIndex[i] + data.length
-        //     }.json`
-        // );
-        // writeStream.write(JSON.stringify(data));
+        result2[i].push([data[0], data[data.length - 1], data.length]);
       }
 
       if (currentIndex[i] + 30 >= endIndex[i]) currentIndex[i] = endIndex[i];
@@ -115,65 +83,87 @@ function multiSplit() {
     }
   }
 
-  // fs.appendFileSync(
-  //   __dirname + `/result1.json`,
-  //   JSON.stringify(result2),
-  //   (err) => {
-  //     if (err) {
-  //       console.log("49", err);
-  //     }
-  //     // console.log(result2);
-  //   }
-  // );
-
+  // console.log(result2);
   return result2;
 }
 
 ///////// Get ranges from group of 30 words ////////////
+function getNewItem(result2, index) {
+  while (
+    result2[index] != undefined &&
+    result2[index].length == 0 &&
+    index < result2.length
+  ) {
+    index += 1;
+  }
+  if (index < result2.length) return index;
+  return -1;
+}
+
 function getRanges(result2) {
-  let ranges = [];
-  let group = [],
+  console.log("Result2");
+  console.log(result2);
+  for (let i = 0; i < result2.length; i++)
+    for (let j = 0; j < result2[i].length; j++) {
+      result2[i][j].splice(2, 0, i);
+    }
+
+  let ranges = [],
+    group = [],
     findIndexes = [0, 0, 0, 0];
 
+  function insertIntoRanges(newGroups) {
+    newGroups.forEach((grp) => {
+      grp.sort();
+    });
+  }
+
+  function getRanges() {
+    // console.log();
+  }
   for (let i = 0; i < 4; i++) {
-    group.push(result2[i][0].slice(0, 2));
+    group.push(result2[i][0].slice(0, 3));
+    result2[i].splice(0, 1);
     findIndexes[i] += 1;
   }
 
-  console.log(group);
   while (group.length > 0) {
+    // console.log("Groups");
+    // console.log(group);
     group.sort();
     let operate = group[0];
     group.splice(0, 1);
-
+    // console.log(operate);
     try {
       let result = operate[0].localeCompare(ranges[ranges.length - 1][2]);
       if (result == -1) {
-        let last = ranges[ranges.length - 1];
+        let last = ranges[ranges.length - 1],
+          newGroups = [[last[0], operate[0]]];
+
+        if (last[1].localeCompare(operate[1]) == -1)
+          newGroups.push([last[1], operate[1]]);
+        else newGroups.push([operate[1], last[1]]);
+
         ranges.pop();
-        let newGroups = [
-          [operate[0], last[0]],
-          [operate[1], last[1]],
-        ];
-        // console.log(newGroups);
-        newGroups.forEach((grp) => {
-          ranges.push(grp);
-        });
+        insertIntoRanges(newGroups);
       } else throw "error";
     } catch (error) {
-      // console.log("Error called");
       ranges.push(operate.slice(0, 2));
+    }
+    let newItemIndex = getNewItem(result2, operate[2]);
+    if (newItemIndex != -1) {
+      group.push(result2[newItemIndex][0].slice(0, 3));
+      result2[newItemIndex].splice(0, 1);
     }
 
     // console.log(group);
     // console.log(ranges);
     // console.log("============");
   }
+  console.log("Ranges");
   console.log(ranges);
 }
 
 ///////// START HERE /////////
-
-// splitDataIntoChunks();
-let R2 = multiSplit();
-getRanges(R2);
+splitDataIntoChunks();
+getRanges(multiSplit());
